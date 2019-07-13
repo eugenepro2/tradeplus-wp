69390d244c8ad4bb9bec49ef4ad365f6cb247216
Eugene Pro <eugenepro@icloud.com>
2019-07-13 13:04:28 +0300

    Revert "upload"
    This reverts commit cb0854e4edac8fa2851647a510e4b605b1e979ea.

--- wp-content/themes/tradeplus/assets/src/js/app.js
+++
@@ -1,7 +0,0 @@
-import './import/menu';
-import './import/slider';
-import './import/barba';
-import './import/map';
-import './import/slideout';
-import './import/line';
-import objectFitImages from 'object-fit-images';
--- wp-content/themes/tradeplus/assets/src/js/import/slideout.js
+++
@@ -1,8 +0,0 @@
-import Slideout from 'slideout';
-import $ from 'jquery';
-
-$('.menu-button').on('click', function() {
-  $(this).find('span').toggleClass('active');
-  $('.header__mobile').toggleClass('active');
-  $('body').toggleClass('active');
-});
